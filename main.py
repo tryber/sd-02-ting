666b82d395da6b8b39cf70dc3d65879ab0f69b7e
phtf92 <phtf92@gmail.com>
2020-10-26 18:13:56 -0300

    Ex1

---
+++ main.py
@@ -0,0 +1,4 @@
+from ting_file_management import file_management
+
+
+file_management.txt_importer('statics/arquivo_teste.txt')
--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,24 @@
+import os.path
+import sys
+
+
+def check_comparison(item, item_to_check, exception_message):
+    if item != item_to_check:
+        raise ValueError(exception_message)
+
 def txt_importer(path_file):
-    raise NotImplementedError
+    try:
+        with open(path_file, "r") as file:
+            extension = os.path.splitext(path_file)[1]
+            check_comparison(extension, ".txt", "Formato inválido")
+            content = file.read().split(sep = '\n')
+            print(content)
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    except ValueError as exc_ext:
+        print(exc_ext, file=sys.stderr)
+
+    else:
+        print("Importação realizada com sucesso", file=sys.stdout)
