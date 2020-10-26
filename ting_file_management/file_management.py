d57a12fb2b3be0406abb2fbdf6db1fe5167c3148
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-10-26 18:37:10 -0300

    Requisito 1

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    raise NotImplementedError
+    text_list = []
+    with open(path_file, 'r') as text:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+            return
+        content = text.read()
+        for line in content.split('\n'):
+            text_list.append(line)
+    return text_list
