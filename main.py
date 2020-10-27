6d8d5533725a38b5ae9c43b4f4e8cce976b6966e
phtf92 <phtf92@gmail.com>
2020-10-27 20:16:08 -0300

    Ex 5 e 6

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ting_file_management.file_process import process, remove, file_metadata
 from ting_file_management.Deque import Deque
+from ting_word_searches.word_search import search_by_word, exists_word
 
 
 project_instance = Deque()
@@ -22,5 +23,6 @@ print('-------')
 file_metadata(project_instance, 2)
 print('-------')
 project_instance.peek_back()
-
-# file_management.txt_importer('statics/arquivo_teste.txt')
+print('------->>')
+print(search_by_word('Pedro', project_instance))
+print(exists_word('Pedro', project_instance))
