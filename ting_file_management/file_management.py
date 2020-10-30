1ad58b152a4d853ef3aeea633a6bb29d0465625d
eddyeboy27 <lipe0503@gmail.com>
2020-10-30 18:57:59 -0300

    Atualizado txt_importer e process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,7 @@
 def txt_importer(path_file):
-    raise NotImplementedError
+    text_list = []
+    with open(path_file, "r") as text:
+        text_read = text.read()
+        for line in text_read.split('\n'):
+            text_list.append(line)
+    return text_list
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,31 @@
+import sys
+from os import path
+from file_management import txt_importer
+
+
 def process(path_file):
-    raise NotImplementedError
+    exist = path.exists(path_file) or path.exists(f"./{path_file}")
+    list_text = []
+    line_count = 0
+
+    if not exist:
+        print(f"Arquivo {path_file} não existe", file=sys.stderr)
+        return
+    if not path_file.endswith(".txt"):
+        print("Formato Inválido", file=sys.stderr)
+        return
+
+    text_read = txt_importer(path_file)
+
+    for line in text_read:
+        line_count += 1
+    list_text.append({
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": line_count,
+        "linhas do arquivo": text_read
+    })
+
+    return list_text
 
 
 def remove():
@@ -8,3 +34,6 @@ def remove():
 
 def file_metadata(position):
     raise NotImplementedError
+
+
+print(process("./statics/arquivo_teste.txt"))
