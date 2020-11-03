be67846b8b643f5dc50e5086475b0b9f0881b598
Miguel Miranda <miguelmirandaazevedo@gmail.com>
2020-11-03 10:58:23 -0300

    Pacote ting_word_searches implementado

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from collections import deque as Deque
 from ting_file_management.file_process import process, remove, file_metadata
+from ting_word_searches.word_search import exists_word, search_by_word
 
 deque = Deque()
 
 process("arquivo_nao_existente.txt", deque)
 process("statics/arquivo_nao_txt.json", deque)
-print(deque)
 
 process("statics/novo_paradigma_globalizado-min.txt", deque)
 process("statics/arquivo_teste.txt", deque)
 process("statics/arquivo_teste.txt", deque)
-print(deque)
 
-remove(deque)
-remove(deque)
-remove(deque)
-print(deque)
-
-process("statics/arquivo_teste.txt", deque)
 file_metadata(0, deque)
 file_metadata(1, deque)
+file_metadata(2, deque)
+
+exists_word("variáveis", deque)
+exists_word("tudo", deque)
+exists_word("cassio", deque)
+
+search_by_word("Variáveis", deque)
+search_by_word("tuDo", deque)
+search_by_word("cassiO", deque)
+
+remove(deque)
+remove(deque)
+remove(deque)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,2 +1,37 @@
-def search_by_word(word):
-    raise NotImplementedError
+def exists_word(word, deque):
+    file_results = []
+    for item in deque:
+        line_results = []
+        for index, line in enumerate(item["linhas_do_arquivo"]):
+            if word.upper() in line.upper():
+                line_results.append({
+                    "linha": index + 1,
+                })
+        if len(line_results) > 0:
+            file_results.append({
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": line_results,
+            })
+    print(file_results)
+    return file_results
+
+
+def search_by_word(word, deque):
+    file_results = []
+    for item in deque:
+        line_results = []
+        for index, line in enumerate(item["linhas_do_arquivo"]):
+            if word.upper() in line.upper():
+                line_results.append({
+                    "linha": index + 1,
+                    "conteudo": line,
+                })
+        if len(line_results) > 0:
+            file_results.append({
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": line_results,
+            })
+    print(file_results)
+    return file_results
