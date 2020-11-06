8cc6ae15090b9d296d054f29d4bd17d0f9e53ad9
Lauro Lyra Aguiar <lauro.aguiar@pagueveloz.com.br>
2020-11-05 23:21:42 -0300

    txt_importer ok

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
 def txt_importer(path_file):
-    raise NotImplementedError
+    try:
+        with open(path_file, "r") as file:
+            if not path_file.endswith(".txt"):
+                print("Formato inválido", file=sys.stderr)
+                sys.exit(1)
+            file_split = file.read().split(sep="\n")
+            return file_split
+        print("Importação realizada com sucesso")
+
+    except ValueError as exc_ext:
+        print(exc_ext, file=sys.stderr)
+        sys.exit(1)
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        sys.exit(1)
