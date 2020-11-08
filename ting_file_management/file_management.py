f153fe31e6139cc4d7aad0f95fe3c069f1de7864
Lauro Lyra Aguiar <laurolyra@gmail.com>
2020-11-08 15:37:55 -0300

    file_process ok

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,17 +1,17 @@
 import sys
 
+
 def txt_importer(path_file):
     try:
         with open(path_file, "r") as file:
             if not path_file.endswith(".txt"):
-                print("Formato inválido", file=sys.stderr)
-                sys.exit(1)
+                raise ValueError("Formato inválido")
             file_split = file.read().split(sep="\n")
             return file_split
-        print("Importação realizada com sucesso")
+        print("Importação realizada com sucesso", file=sys.stdout)
 
-    except ValueError as exc_ext:
-        print(exc_ext, file=sys.stderr)
+    except ValueError as val_exc:
+        print(val_exc, file=sys.stderr)
         sys.exit(1)
 
     except FileNotFoundError:
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+from collections import deque # thx julio
+import sys
+
+queue = deque()
+
+
 def process(path_file):
-    raise NotImplementedError
+    data = txt_importer(path_file)
+    if (data is None):
+        return
+    file_name = path_file.split('/')
+    file = {
+        'nome_do_arquivo': file_name[len(file_name) - 1],
+        'qtd_linhas': len(data),
+        'linhas_do_arquivo': data
+    }
+    if (file not in queue):
+        queue.append(file)
+        print(file, file=sys.stdout)
 
 
 def remove():
