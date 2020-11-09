cecb993673e227e4770e1b082bd96d946d4efac9
guicgs <etohlif@protonmail.com>
2020-11-09 19:45:38 -0300

    requisito 4 concluído

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from ting_file_management.file_process import process, remove
+from ting_file_management.file_process import process, remove, file_metadata
 
-process("statics/arquivo_teste.txt")
 process("statics/novo_paradigma_globalizado.txt")
 process("statics/novo_paradigma_globalizado-min.txt")
 process("statics/arquivo_teste.txt")
+process("statics/arquivo_teste.txt")
 
 remove()
 remove()
+
+file_metadata(0)
+file_metadata(1)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,3 +1,4 @@
+import sys
 from ting_file_management.file_management import txt_importer
 from collections import deque as Deque
 
@@ -10,7 +11,7 @@ def process(path_file):
     file_process = {
         "nome_do_arquivo": path_file.split("/")[-1],
         "qtd_linhas": len(text_lines),
-        "linhas_do_arquivo": text_lines
+        "linhas_do_arquivo": text_lines,
     }
     if file_process not in deque:
         deque.append(file_process)
@@ -25,4 +26,10 @@ def remove():
 
 
 def file_metadata(position):
-    raise NotImplementedError
+    try:
+        print(deque[position])
+        return deque[position]
+    # solução encontrada em:
+    # https://www.tutorialspoint.com/How-to-catch-IndexError-Exception-in-Python
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
