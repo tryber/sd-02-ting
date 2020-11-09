cb2099b63140ebc771ae5a87238ccc4247e82ede
guicgs <etohlif@protonmail.com>
2020-11-09 18:43:30 -0300

    requisito 1 concluído

---
+++ main.py
@@ -0,0 +1,3 @@
+from ting_file_management.file_management import txt_importer
+
+print(txt_importer("statics/arquivo_teste.txt"))
--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
+def check_file_extension(file):
+    if not file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+        return False
+    return True
+
+
 def txt_importer(path_file):
-    raise NotImplementedError
+    try:
+        with open(path_file, "r") as text:
+            if check_file_extension(path_file):
+                return [line.split('\n')[0] for line in text]
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
